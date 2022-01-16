ccabf72f33c3f58da6067961f1f0f003b177de03
felipecabesssa <felipesoares100@hotmail.com>
2022-01-16 16:34:19 -0300

    Inicio do projeto Loja/TrocaTroca

---
+++ trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/TrocatrocabksApplication.java
@@ -0,0 +1,13 @@
+package br.com.bankslife.trocatrocabks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocatrocabksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrocatrocabksApplication.class, args);
+	}
+
+}
---
+++ trocatrocabks/src/test/java/br/com/bankslife/trocatrocabks/TrocatrocabksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bankslife.trocatrocabks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrocatrocabksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
