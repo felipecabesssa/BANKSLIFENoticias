fc29208c15fe9b9b62c123165364a4af6aaafece
felipecabesssa <felipesoares100@hotmail.com>
2022-01-19 21:59:51 -0300

    Erro 404 - Voltando pra Home - ExceptionHandler(IlegalArg...

--- Backend/trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/controller/HomeController.java
+++ Backend/trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,11 @@ public class HomeController {
 		List<Produto> produtos = produtoRepository.findByStatus(StatusProduto.valueOf(status.toUpperCase()));
 		model.addAttribute("produtos", produtos);		
 		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 
 }
