417e25c7c60c5e419d019fade44dc17b2b898b11
felipecabesssa <felipesoares100@hotmail.com>
2022-01-19 21:52:35 -0300

    Filtrando pelo status - Passando o status na URL

--- Backend/trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/controller/HomeController.java
+++ Backend/trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/controller/HomeController.java
@@ -6,21 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.bankslife.trocatrocabks.modelo.Produto;
+import br.com.bankslife.trocatrocabks.modelo.StatusProduto;
 import br.com.bankslife.trocatrocabks.repository.ProdutoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 		
 		List<Produto> produtos = produtoRepository.findAll();
+		model.addAttribute("produtos", produtos);		
+		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String poStatus(@PathVariable("status") String status, Model model) {
+		List<Produto> produtos = produtoRepository.findByStatus(StatusProduto.valueOf(status.toUpperCase()));
 		model.addAttribute("produtos", produtos);		
 		return "home";
 	}
--- Backend/trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/repository/ProdutoRepository.java
+++ Backend/trocatrocabks/src/main/java/br/com/bankslife/trocatrocabks/repository/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package br.com.bankslife.trocatrocabks.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.bankslife.trocatrocabks.modelo.Produto;
+import br.com.bankslife.trocatrocabks.modelo.StatusProduto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	List<Produto> findByStatus(StatusProduto status);
 		
 }
