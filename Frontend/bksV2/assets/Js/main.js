2e4e11532ae98a0201f36aa90fed9e58fa80e03b
felipecabesssa <felipesoares100@hotmail.com>
2022-02-22 22:08:46 -0300

    Alteracao aplicada aos outros videos - Lista ok

--- Frontend/bksV2/assets/Js/main.js
+++ Frontend/bksV2/assets/Js/main.js
@@ -12,46 +12,35 @@ const btnVideoPico = document.querySelector('.btnVideoPico');
 const videoDestaqueBlacYellow = document.querySelector('.videoDestaqueBlacYellow');
 const btnVideoBlackYellow = document.querySelector('.btnVideoBlackYellow');
 
+
+
 btnVideoBala.addEventListener('click', function(event) {
-    alteraVideo();
+    alteraVideoBala();
 });
 
 btnVideoPico.addEventListener('click', function(event) {
-    alert('Video Pico selecionado');
+    alteraVideoPico();
 });
 
 btnVideoBlackYellow.addEventListener('click', function(event) {
-    alert('Video Black and yellow - Wiz Kalifa selecionado');
+    alteraVideoBlacYellow();
 });
 
-function alteraVideo() {
+function alteraVideoBala() {
     videoDestaqueBala.classList.remove('videoEscondido');
     videoDestaquePico.classList.add('videoEscondido');
     videoDestaqueBlacYellow.classList.add('videoEscondido');
-
 }
 
+function alteraVideoPico() {
+    videoDestaquePico.classList.remove('videoEscondido');
+    videoDestaqueBala.classList.add('videoEscondido');
+    videoDestaqueBlacYellow.classList.add('videoEscondido');
+}
 
-
-
-
-// btnVideo.addEventListener('click', function (e) {
-//     const el = e.target;
-
-//     if (el.classList.contains('videoBala')) {
-//         videoDestaque.removeAttribute('class', 'videoEscondido');
-//     }
-
-    // if (el.classList.contains('videoBala')) {
-    //     videoDestaque.classList.remove('videoEscondido') 
-    //     || videoDestaque.classList.add('mostraVideo');
-    // }
-
-    // if (el.classList.contains('videoBlackYellow')) {
-    //     videoDestaque.classList.remove('videoEscondido') 
-    //     || videoDestaque.classList.add('mostraVideo');
-    // }
-
-// });
-
+function alteraVideoBlacYellow() {
+    videoDestaqueBlacYellow.classList.remove('videoEscondido');
+    videoDestaquePico.classList.add('videoEscondido');
+    videoDestaqueBala.classList.add('videoEscondido');
+}
 
