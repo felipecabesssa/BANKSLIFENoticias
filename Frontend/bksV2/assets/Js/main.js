a0e579747e2bd2e4bde567b1004d688f0ad2b898
felipecabesssa <felipesoares100@hotmail.com>
2022-02-22 23:02:36 -0300

    Alterando videos da lista

--- Frontend/bksV2/assets/Js/main.js
+++ Frontend/bksV2/assets/Js/main.js
@@ -1,46 +1,59 @@
 // onclick setAtribute(mostraVideo) || setAtribute(escondeVideo) || setAtribute(escondeVideo)
 //                  videoClicado                outro video                  outro video
 
-const videoDestaque = document.querySelector('.videoDestaque');
+const videoDestaquePico0 = document.querySelector('.videoDestaquePico0');
+const btnVideoBala = document.querySelector('.btnVideoPico0');
 
-const videoDestaqueBala = document.querySelector('.videoDestaqueBala');
-const btnVideoBala = document.querySelector('.btnVideoBala');
+const videoDestaquePico1 = document.querySelector('.videoDestaquePico1');
+const btnVideoPico1 = document.querySelector('.btnVideoPico1');
 
-const videoDestaquePico = document.querySelector('.videoDestaquePico');
-const btnVideoPico = document.querySelector('.btnVideoPico');
+const videoDestaquePico2 = document.querySelector('.videoDestaquePico2');
+const btnVideoPico2 = document.querySelector('.btnVideoPico2');
 
-const videoDestaqueBlacYellow = document.querySelector('.videoDestaqueBlacYellow');
-const btnVideoBlackYellow = document.querySelector('.btnVideoBlackYellow');
-
+const videoDestaquePico3 = document.querySelector('.videoDestaquePico3');
+const btnVideoPico3 = document.querySelector('.btnVideoPico3');
 
 
 btnVideoBala.addEventListener('click', function(event) {
-    alteraVideoBala();
+    alteraVideoPico0();
 });
 
-btnVideoPico.addEventListener('click', function(event) {
-    alteraVideoPico();
+btnVideoPico1.addEventListener('click', function(event) {
+    alteraVideoPico1();
 });
 
-btnVideoBlackYellow.addEventListener('click', function(event) {
-    alteraVideoBlacYellow();
+btnVideoPico2.addEventListener('click', function(event) {
+    alteraVideoPico2();
 });
 
-function alteraVideoBala() {
-    videoDestaqueBala.classList.remove('videoEscondido');
-    videoDestaquePico.classList.add('videoEscondido');
-    videoDestaqueBlacYellow.classList.add('videoEscondido');
+btnVideoPico3.addEventListener('click', function(event) {
+    alteraVideoPico3();
+});
+
+function alteraVideoPico0() {
+    videoDestaquePico0.classList.remove('videoEscondido');
+    videoDestaquePico1.classList.add('videoEscondido');
+    videoDestaquePico2.classList.add('videoEscondido');
+    videoDestaquePico3.classList.add('videoEscondido');
 }
 
-function alteraVideoPico() {
-    videoDestaquePico.classList.remove('videoEscondido');
-    videoDestaqueBala.classList.add('videoEscondido');
-    videoDestaqueBlacYellow.classList.add('videoEscondido');
+function alteraVideoPico1() {
+    videoDestaquePico1.classList.remove('videoEscondido');
+    videoDestaquePico0.classList.add('videoEscondido');
+    videoDestaquePico2.classList.add('videoEscondido');
+    videoDestaquePico3.classList.add('videoEscondido');
 }
 
-function alteraVideoBlacYellow() {
-    videoDestaqueBlacYellow.classList.remove('videoEscondido');
-    videoDestaquePico.classList.add('videoEscondido');
-    videoDestaqueBala.classList.add('videoEscondido');
+function alteraVideoPico2() {
+    videoDestaquePico2.classList.remove('videoEscondido');
+    videoDestaquePico1.classList.add('videoEscondido');
+    videoDestaquePico0.classList.add('videoEscondido');
+    videoDestaquePico3.classList.add('videoEscondido');
 }
 
+function alteraVideoPico3() {
+    videoDestaquePico3.classList.remove('videoEscondido');
+    videoDestaquePico1.classList.add('videoEscondido');
+    videoDestaquePico2.classList.add('videoEscondido');
+    videoDestaquePico0.classList.add('videoEscondido');
+}
